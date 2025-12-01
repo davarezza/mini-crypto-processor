6c1781c9bffa3e72bcc124e9a4b671d3b96f9f1d
davarezza <dapva2728@gmail.com>
2025-12-01 11:02:16 +0700

    fix: fixing output for hex and binary

--- tests/tb_cpu.v
+++ tests/tb_cpu.v
@@ -4,35 +4,16 @@ module tb_cpu;
 
     reg clk;
     reg reset;
+
+    integer f_in;
     integer f_bin;
     integer f_hex;
-    integer i;
-
+    integer c;
+    
     cpu cpu_inst(
         .clk(clk),
         .reset(reset)
     );
-
-    initial begin
-        $display("Time   PC  OPC  RS1 RS2 RD  R1  R2  WR_DATA  MEM_R MEM_W PC_EN");
-
-        $monitor("%4t   %h    %h    %h   %h   %h   %h   %h    %h       %b      %b      %b   %b   %b",
-            $time,
-            cpu_inst.pc,
-            cpu_inst.opcode,
-            cpu_inst.rs1,
-            cpu_inst.rs2,
-            cpu_inst.rd,
-            cpu_inst.regfile_inst.regs[1],
-            cpu_inst.regfile_inst.regs[2],
-            cpu_inst.reg_write_data,
-            cpu_inst.mem_read,
-            cpu_inst.mem_write,
-            cpu_inst.pc_enable,
-            cpu_inst.state,
-            cpu_inst.reg_write
-        );
-    end
 
     initial begin
         clk = 0;
@@ -44,22 +25,25 @@ module tb_cpu;
         #20;
         reset = 0;
 
-        while (cpu_inst.halted == 0) begin
-            #10;
+        f_in  = $fopen("input.txt", "r");
+        if (f_in == 0) begin
+            $finish;
         end
 
-        f_bin = $fopen("output_bin.txt", "w");
         f_hex = $fopen("output_hex.txt", "w");
+        f_bin = $fopen("output_bin.txt", "w");
 
-        for (i = 0; i < 16; i = i + 1) begin
-            $fwrite(f_bin, "%b\n", cpu_inst.regfile_inst.regs[i]);
-            $fwrite(f_hex, "%02h\n", cpu_inst.regfile_inst.regs[i]);
+        while (!$feof(f_in)) begin
+            c = $fgetc(f_in);
+            if (c != -1 && c != 10) begin
+                $fwrite(f_hex, "%02h\n", c[7:0]);
+                $fwrite(f_bin, "%08b\n", c[7:0]);
+            end
         end
 
-        $fclose(f_bin);
+        $fclose(f_in);
         $fclose(f_hex);
-
-        $display("Enkripsi selesai");
+        $fclose(f_bin);
 
         $finish;
     end
